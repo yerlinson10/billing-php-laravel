fc265fc5dc0c4690d2f8ff6c5531c30f4fb63681
yerlinson <yerlinson.lora@hytechhome.com>
2024-07-17 11:12:44 -0400

    feat: Creation of migrations, models and controllers.

---
+++ database/migrations/2024_07_17_145751_create_invoices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoices', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->nullOnDelete();
+            $table->foreignId('customer_id')->nullOnDelete();
+            $table->string('subtotal');
+            $table->string('totaltax');
+            $table->string('total_discount');
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoices');
+    }
+};
---
+++ database/migrations/2024_07_17_150257_create_invoice_details_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('invoice_details', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('invoice_id')->nullOnDelete();
+            $table->foreignId('product_id')->nullOnDelete();
+            $table->string('subtotal');
+            $table->string('totaltax');
+            $table->string('total');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('invoice_details');
+    }
+};
